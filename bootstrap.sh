7ce33cbf197bb74114982cc3b7cbb46a0d190589
Hugues Marilleau <hugues@marilleau.com>
2013-08-09 18:12:11 +0200

    Move git aliases to .gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,7 @@ fi
 ln -f -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -f -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -f -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -f -s ~/.dotfiles/.vimrc ~/.vimrc
 if [ -d ~/.vim/bundle/vundle ]; then
   cd ~/.vim/bundle/vundle; git pull origin
